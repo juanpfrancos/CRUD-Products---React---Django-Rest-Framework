f8b7b99fc81bb7189bd8abdfc6be167cb8c6e439
juanpfrancos <juanpanime@gmail.com>
2020-11-12 21:03:35 -0500

    Add git pages config.

--- Front React/src/components/listar.jsx
+++ Front React/src/components/listar.jsx
@@ -1,15 +1,14 @@
 import React, {useState, useEffect} from 'react'
-import Button from '@material-ui/core/Button';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
 
 function Listar(){
 
@@ -50,7 +49,7 @@ const fetchData = () => {
               <Button variant="contained" color="primary" type="submit" component={Link} to="/add" className={classes.button}>
                   Agregar Producto
               </Button>
-              <TableContainer component={Paper}>
+              <TableContainer>
                 <Table className={classes.table} aria-label="simple table">
                   <TableHead>
                     <TableRow>
